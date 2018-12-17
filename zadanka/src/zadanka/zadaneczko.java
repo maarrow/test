c6cce711dd294b6c9c2b704791b9a658265bb8f0
Marek <Marek@192.168.1.13>
2018-12-17 21:38:16 +0100

    first commit

---
+++ zadanka/src/zadanka/zadaneczko.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package zadanka;
+
+/**
+ * @author Marek
+ *
+ */
+public class zadaneczko {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+int pierwsza=20;
+int druga=10;
+int trzecia=30;
+System.out.println("najwiêksza liczb¹ jest " + Math.max(pierwsza, Math.max(druga, trzecia)));
+
+	}
+}
